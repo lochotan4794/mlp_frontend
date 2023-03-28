7bf13f003325495fb4e08a97c4f55c609155e756
loc <htanloc994@gmail.com>
2023-03-28 16:00:02 +0700

    change ui

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,10 +2,10 @@ export default function Menu() {
     return (
         <>
             <div className="dropdown-menu-1">
-                <a href="/asr">Automatic Speech Recognition</a>
-                <a href="/nlp">Natural Language Processing</a>
-                <a href="/cv">Computer Vision</a>
-                <a href="/other">Others</a>
+                <a className="topic-dropdown-item" href="/asr">Automatic Speech Recognition</a>
+                <a className="topic-dropdown-item" href="/nlp">Natural Language Processing</a>
+                <a className="topic-dropdown-item" href="/cv">Computer Vision</a>
+                <a className="topic-dropdown-item" href="/other">Others</a>
             </div>
         </>
     )
